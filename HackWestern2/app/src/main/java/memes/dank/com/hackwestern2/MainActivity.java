a8154e36cead1aac79fba457291e6a9020b9eb6e
Akshay Pall <iosakshay@gmail.com>
2015-11-28 09:03:54 -0500

    Data Query Test

--- HackWestern2/app/src/main/java/memes/dank/com/hackwestern2/MainActivity.java
+++ HackWestern2/app/src/main/java/memes/dank/com/hackwestern2/MainActivity.java
@@ -3,11 +3,17 @@ package memes.dank.com.hackwestern2;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.FindCallback;
+import com.parse.LogInCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -18,19 +24,48 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String USER_POINTS = "Points";
+
+    //for the song class
+    private static final String CLASS_SONG = "Song";
+    private static final String SONG_PLAYS = "Plays";
+    private static final String SONG_NAME = "Name";
+    private static final String SONG_ARTIST = "Artist";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Parse.enableLocalDatastore(this);
-
         Parse.initialize(this, "rEopROdu5tjjipAGOzcM40l9FO89tO0CyTTrJa6v", "xG5LPIGLxpeacda1audJUZwI1dP2naXjt6JsKfHl");
 
+        ParseUser.logInInBackground("John Cena", "password", new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                //TODO: query points, song plays
 
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
+                //logging parse data
+
+                Log.wtf("User Name", ""+ParseUser.getCurrentUser().getUsername());
+                Log.wtf("User Points", ""+ParseUser.getCurrentUser().get(USER_POINTS));
+
+                ParseQuery<ParseObject> songQuery = ParseQuery.getQuery(CLASS_SONG);
+                songQuery.setLimit(3);
+                songQuery.addDescendingOrder(SONG_PLAYS);
+                songQuery.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if (e == null){
+                            for (int i = 0; i < objects.size() ; i++){
+                                ParseObject song = objects.get(i);
+                                Log.wtf("Song Info", "Plays: "+song.get(SONG_PLAYS)+", "+song.get(SONG_NAME)+" by "+song.get(SONG_ARTIST));
+                            }
+                        }
+                    }
+                });
+            }
+        });
     }
 
     @Override
