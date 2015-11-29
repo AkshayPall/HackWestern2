2628ad11c6b157e36f185f699544e978ebbc23f3
AvikHasija <avikhasija@gmail.com>
2015-11-29 07:10:50 -0500

    Done Button Added to log in

--- HackWestern2/app/src/main/java/memes/dank/com/hackwestern2/LoginActivity.java
+++ HackWestern2/app/src/main/java/memes/dank/com/hackwestern2/LoginActivity.java
@@ -1,7 +1,10 @@
 package memes.dank.com.hackwestern2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
@@ -22,6 +25,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button doneButton = (Button) findViewById(R.id.done_button);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
 
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
